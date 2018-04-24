049b8aa554e47b1fe2463ea111931f32b2f9b946
napier40401617 <dragonwar2024@gmail.com>
2018-04-24 09:48:04 +0630

    Alphabetic

--- server.py
+++ server.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request
 import json
 
-
+page_number =10
 w = json.load(open("worldl.json"))
+lota=sorted(list(set([c['name'][0] for c in w])))
+print(lota)
+
 for c in w:
 	c['tld'] = c['tld'][1:]
 page_size = 20
@@ -13,7 +16,8 @@ def mainPage():
 	return render_template('index.html',
 		page_number=0,
 		page_size=page_size,
-		w = w[0:page_size])
+		w = w[0:page_size],
+		lota=lota)
 
 @app.route('/begin/<b>')
 def beginPage(b):
@@ -32,6 +36,16 @@ def continentPage(a):
 		length_of_cl = len(cl),
 		cl = cl,
 		a = a
+		)
+@app.route('/alphabetic/<a>')
+def alphabeticPage(a):
+	cl = [c for c in w if c['name'][0]==a]
+	return render_template(
+		'continent.html',
+		length_of_cl = len(cl),
+		cl = cl,
+		a = a,
+		lota=lota
 		)
 
 @app.route('/country/<i>')
@@ -77,6 +91,8 @@ def editcountryByNamePage(n):
 		'country-edit.html',
 		c = c)
 
+
+
 @app.route('/updatecountrybyname')
 def updatecountryByNamePage():
 	n=request.args.get('name')
@@ -88,7 +104,7 @@ def updatecountryByNamePage():
 	c['continent']=request.args.get('continent')
 	c['area']=int (request.args.get('area'))
 	c['population']=int(request.args.get('population'))
-	c['gdp']=int(request.args.get('gdp'))
+	c['gdp']=float(request.args.get('gdp'))
 	c['tld']=str(request.args.get('tld'))
 	return render_template(
 		'country.html',
@@ -112,7 +128,7 @@ def savecountryByNamePage():
 	c['continent']=str(request.args.get('continent'))
 	c['area']=float(request.args.get('area'))
 	c['population']=int(request.args.get('population'))
-	c['gdp']=int(request.args.get('gdp'))
+	c['gdp']=float(request.args.get('gdp'))
 	c['tld']=str(request.args.get('tld'))
 	w.append(c)
 	
